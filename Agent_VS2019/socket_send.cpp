f53eae46e719ba0d8c29fb0e15f4c23c87ccee8d
unknown <james61124@gmail.com>
2023-07-26 10:26:30 +0800

    init

--- Agent_VS2019/socket_send.cpp
+++ Agent_VS2019/socket_send.cpp
@@ -26,6 +26,7 @@ int SocketSend::SendMessageToServer(char* Work, char* Mgs) {
 	strcpy_s(GetServerMessage.UUID, sizeof(GetServerMessage.UUID), info->UUID);
 
 	char WorkNew[sizeof(Work)+1];
+	printf("sizeof work %d", sizeof(Work));
 	strncpy_s(WorkNew, sizeof(WorkNew), Work, sizeof(Work));
 	WorkNew[sizeof(WorkNew) - 1] = '\0';
 	strcpy_s(GetServerMessage.DoWorking, sizeof(GetServerMessage.DoWorking), Work);
