43aa41f6c31c40ecf26d942701035fd30c137fa2
unknown <james61124@gmail.com>
2023-07-27 15:28:35 +0800

    init

--- Agent_VS2019/info.cpp
+++ Agent_VS2019/info.cpp
@@ -8,5 +8,5 @@ Info::Info() {
     tcpSocket = new SOCKET;
     strcpy_s(MAC, sizeof(MAC), "08:00:27:4e:66:ef");
     strcpy_s(IP, sizeof(IP), "127.0.0.1");
-    strcpy_s(UUID, sizeof(UUID), "58f033ed05e34ebb82bb2ac879940ddc");
+    strcpy_s(UUID, sizeof(UUID), "fe369d13f9f44565bad2ef70d8a328a0");
 }
